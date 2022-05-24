af3fd825753ec943cb74a23cf0d9f284a44ccb63
SakVla <V_S_@list.ru>
2022-05-24 19:47:38 +0300

    New home work

---
+++ home_work_HEROES.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+
+def get_heroes():
+    url = 'https://superheroapi.com/api/2619421814940190/search/'
+    heroes_list = ['Hulk', 'Captain America', 'Thanos']
+    intelligence_dict = {'Hulk': 0, 'Captain America': 0, 'Thanos': 0}
+
+    for hero in heroes_list:
+        hero_dict = json.loads(requests.get(url + hero).content)
+        intelligence_dict[hero] = int(hero_dict['results'][0]['powerstats']['intelligence'])
+
+    return intelligence_dict
+
+print(get_heroes())
+
+def most_intelligence():
+    return max(get_heroes(), key=get_heroes().get)
+
+print(f'Cамый умный супергерой - {most_intelligence()}')
